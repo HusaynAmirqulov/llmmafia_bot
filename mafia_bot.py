6804e533c5276d6b5fe55bf2c59cc66055bde721
Husayn Amirqulov <147226406+HusaynAmirqulov@users.noreply.github.com>
2026-01-02 20:16:13 +0500

    Update mafia_bot.py

--- mafia_bot.py
+++ mafia_bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-TOKEN = "BU YERGA TOKENINGNI QO‘Y"
+import os
+TOKEN = os.getenv("TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Salom! Bot ishlayapti ✅")
